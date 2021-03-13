9bbe14609fa570fd57d65245b28f111b1ce95e8a
avatarrrrr <tankoaraujo@gmail.com>
2021-03-13 01:52:56 -0300

    first commit

---
+++ bin/uri_questions.dart
@@ -0,0 +1,5 @@
+import 'package:uri_questions/1000.dart';
+
+void main(List<String> arguments) {
+  print(helloWorld());
+}
---
+++ test/1000_test.dart
@@ -0,0 +1,8 @@
+import 'package:uri_questions/1000.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test("A função deve retornar o clássico 'Hello World!'", () {
+    expect(helloWorld(), 'Hello World!');
+  });
+}
